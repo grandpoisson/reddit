b97cbacd90b8f9ef88792cc4d248c545d1d9c3d5
grandpoisson <grandpoisson@gmail.com>
2022-07-04 08:45:59 +0100

    use consistent header across home, post and subreddit pages

--- pages/r/[subreddit].js
+++ pages/r/[subreddit].js
@@ -10,14 +10,17 @@ export default function Subreddit({ subreddit, posts }) {
   }
 
   return (
-    <>
+    <>  
+      <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
         <Link href={`/`}>
-        <a className='text-center p-5 underline block'>
-          🔙 back to the homepage
-        </a>
-      </Link>
-      <p className='text-center p-5'>/r/{subreddit.name}</p>
-      <Posts posts={posts} />
+            <a className='underline'>Home</a>
+        </Link>
+        <p className='grow'></p>
+      </header>
+      <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
+        <p className='text-center'>/r/{subreddit.name}</p>
+        <p className='ml-4 text-left grow'>{subreddit.description}</p>
+      </header>
     </>
   )
 }
